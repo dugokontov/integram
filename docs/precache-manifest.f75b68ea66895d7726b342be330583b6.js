356cf5808efceacac9496db93d5ba5ba3422d43d
dugokontov <sergejspam@gmail.com>
2020-05-25 11:58:01 +0200

    Add minified code

---
+++ docs/precache-manifest.f75b68ea66895d7726b342be330583b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d863287ecdb8a94f9302c1f3284869b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888ad4b950edd433e6fb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a8564a9cbc635206538b",
+    "url": "/static/js/2.214812f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.214812f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888ad4b950edd433e6fb",
+    "url": "/static/js/main.3378efb4.chunk.js"
+  },
+  {
+    "revision": "1d8ecd25813bec934793",
+    "url": "/static/js/runtime-main.d570392b.js"
+  }
+]);
